4000eed54f5da9231364037acfffda3960dbb28a
ben <benscar3@gmail.com>
2018-11-17 13:27:21 -0500

    initial commit

---
+++ class1.py
@@ -0,0 +1,69 @@
+#######################################################################################
+# make 1 variable storing a string, another storing an int and another storing a boolean
+
+
+#######################################################################################
+# use a print statement to print something (number or string) to the console
+
+
+#######################################################################################
+# use a print statement to print what is stored in a variable to the console
+
+
+
+#######################################################################################
+# store what is held in the variable var1 into the variable var2
+# print what is stored in each to show your results
+
+
+
+#######################################################################################
+# create a string variable holding your name
+
+
+#######################################################################################
+# if you have 2 strings and one stores "dog" and the other stores "cat", add them together
+
+string1 = "dog"
+string2 = "cat"
+
+
+#######################################################################################
+# 3 variables a, b, c
+# if a holds a 1, b holds 2, and c holds 3, get the sum of the variables and print it
+
+print()
+
+#######################################################################################
+# take a variable holding a boolean and change it to the other condition
+# ex. True -> False, False -> True
+
+boolean_var = True
+boolean_var2 = False
+
+
+#######################################################################################
+# after every problem it still prints out the answers after you have moved on
+# use comments (#) to comment out the code and see what happens
+# comments need to be placed in front of the code
+# ex. commenting out the variable var123 = 10 would look like below:
+
+# var123 = 10
+
+#######################################################################################
+# using inequalities determine True or False whether 5 is less than 10
+
+
+#######################################################################################
+# using inequalities determine True or False whether 10 is less than or equal to 10
+
+
+#######################################################################################
+# make inequalities which all evaluate to True using <=, >=, ==, !=
+
+
+#######################################################################################
+# make inequalities which all evaluate to False using <=, >=, ==, !=
+
+#######################################################################################
+# use and, or, not to combine inequalities using ()
