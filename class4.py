704dab34cd551d093057184ca2466e0a77d24537
unknown <benscar3@gmail.com>
2018-12-21 12:26:11 -0500

    added a guide to sync forks to most updated version + other edits

--- class4.py
+++ class4.py
@@ -1,21 +1,26 @@
-##############################################################
+#######################################################################################
 #4.5
 
-# Triangles have exactly three sides. If a triangle has three equal sides it is called an equilateral triangle.
-# If a triangle has exactly two equal sides it is called an isosceles triangle.
-# If a triangle has three unequal sides it is called a scalene triangle.
-# The sides of a triangle must have a certain relationship to one another in order for them to form a valid triangle.
-# The relationship is that the sum of any two sides must be strictly greater than the third side.
-# And obviously, sides must be a whole number.
+# Triangles have exactly three sides. If a triangle has three equal sides it is called 
+# an equilateral triangle. If a triangle has exactly two equal sides it is called an 
+# isosceles triangle. If a triangle has three unequal sides it is called a scalene triangle.
+# The sides of a triangle must have a certain relationship to one another in order for 
+# them to form a valid triangle. The relationship is that the sum of any two sides must 
+# be strictly greater than the third side. Sides must be a whole number.
 
-# Write a program that prompts the user to enter some integer values for the 3 sides of a triangle
-# and then displays the values and the type of triangle they represent.
-# If the user enters values that do not make a valid triangle, or if any values are not greater than zero,
-# then the program will invalidate that triangle by saying: “This isn’t a real triangle!”  
+# Write a program that prompts the user to enter some integer values for the 3 sides 
+# of a triangle and then displays the values and the type of triangle they represent.
+# If the user enters values that do not make a valid triangle, or if any values are not 
+# greater than zero, then the program will invalidate that triangle by saying: 
+# “This isn’t a real triangle!”  
 
-################################################################
+#######################################################################################
 #4.6
 
 # Write a program to solve quadratic equations (use if, else if and else).
 # If there are no real solutions, just say "No Real Solutions!"
-# And remember, to determine if the solutions are real, just make sure the discriminate (b^2 - 4*a*c) is greater than or equal to zero. 
+# And remember, to determine if the solutions are real, just make sure the discriminate 
+# (b^2 - 4*a*c) is greater than or equal to zero. 
+# useful math functions:
+# math.pow - get powers of numbers - ex: math.pow(2, 2) = 4
+# math.sqrt - get square root of number - ex: math.sqrt(4) = 2
