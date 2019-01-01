0de46aea87db50522896e498ba5719141ae4ae5a
ben <benscar3@gmail.com>
2019-01-01 15:11:00 -0500

    added q and a for class 6-8

---
+++ class6.py
@@ -0,0 +1,41 @@
+#######################################################################################
+# 6.1
+# Make a while loop with a counter variable which counts up to 100
+
+
+#######################################################################################
+# 6.2
+# Get the input of 10 numbers using the input function and a while loop. Then take the
+# average of those 10 numbers.
+
+#######################################################################################
+# 6.3
+# Make a while loop program which can calculate the factorial of a number. 3! means 3
+# factorial. 3! = 3*2*1, 4! = 4*3*2*1, 5! = 5*4*3*2*1.
+
+#######################################################################################
+# 6.4
+# Write a program that calculates the greatest common factor(GCF) of two numbers. This
+# is the largest number both numbers can be divided evenly by. To check if a certain 
+# number divides another evenly, use the modulus operator(%). For example, 10 % 5 == 0,
+# 4 % 2 == 0, 10 % 3 == 1, 6 % 4 == 2, 8 % 5 == 3, 100 % 20 == 0.
+
+#######################################################################################
+# 6.5
+# Write a program which continually asks the user for a number until they enter they
+# type "stop". Once they have typed "stop" the computer should output the average
+# of the numbers entered.
+
+#######################################################################################
+# 6.6
+# Using a while loop and strings, make a program which prints out a staircase with a 
+# certain amount of steps. Ask the user how many steps they want using the input 
+# function. For example a staircase with 2 steps would look like this:
+# *
+# **
+# A staircase with 4 steps would look like this:
+# *
+# **
+# ***
+# ****
+# Remember that you can add strings together. For example "race" + "car" = "racecar"
